01fee13e450e6f54061d6489185d3e1318268bad
BalazsParducz <parducz.balazs@gmail.com>
2018-11-08 11:18:47 +0100

    methods to create cards

--- src/main/main.js
+++ src/main/main.js
@@ -1,8 +1,32 @@
 const startButton = document.getElementById('startButton');
+const table = document.getElementById('table');
+const images = [
+    'src/resources/assets/images/cards/angular.png',
+    'src/resources/assets/images/cards/d3.png',
+    'src/resources/assets/images/cards/jenkins.png',
+    'src/resources/assets/images/cards/postcss.png',
+    'src/resources/assets/images/cards/react.png'
+];
+const cards = [];
+
+function makeCard(imagePath){
+    const cardElem = document.createElement('img');
+    cardElem.setAttribute('src', imagePath);
+    cardElem.setAttribute('class', 'cards');
+    return cardElem;
+}
+
+function generateCards() {
+    for(let image in images) {
+        cards.push(makeCard(image));
+        cards.push(makeCard(image))
+    }
+}
+
 
 const game = {
     start(){
-        console.log("test click");
+        generateCards();
     }
 };
 
